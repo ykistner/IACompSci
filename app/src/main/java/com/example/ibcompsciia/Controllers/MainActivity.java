8f201e4d871f10b8b7b2c890ff6beff06132efc8
yuutokistnercompsci <Yuuto_Kistner@fis.edu>
2022-06-15 11:11:45 +0200

    second commit

--- app/src/main/java/com/example/ibcompsciia/Controllers/MainActivity.java
+++ app/src/main/java/com/example/ibcompsciia/Controllers/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.ibcompsciia;
+package com.example.ibcompsciia.Controllers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.ibcompsciia.R;
 
 public class MainActivity extends AppCompatActivity {
 
